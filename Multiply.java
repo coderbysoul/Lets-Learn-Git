a40be39110686d66013de72b67ca7699f0064784
Arjya <arjyabhattacharjee5@gmail.com>
2022-03-10 23:35:46 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+public class Multiply{
+
+  Run|Debug
+  public static void main(String args[]) {
+      int a= 4;
+      int b=2;
+
+      System.out.printlln("the product is " + (a+b));
+  }
+  }
